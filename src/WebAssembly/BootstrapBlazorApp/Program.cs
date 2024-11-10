c4adeb3d30c1c047325f87e606c26ebf5204fac3
Argo-AsicoTech <argo@live.ca>
2024-11-10 19:18:17 +0800

    feat: web app wasm 模式升级到 net9

---
+++ src/WebAssembly/BootstrapBlazorApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using BootstrapBlazorApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddBootstrapBlazor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BootstrapBlazorApp.Client._Imports).Assembly);
+
+app.Run();
