99758620cfc96e343cdec3a013242aba54875984
Argo-AsicoTech <argo@live.ca>
2024-11-23 15:03:58 +0800

    refactor: 增加 CollapseBar 组件

---
+++ src/Auto-Per/BootstrapBlazorApp/Components/Layout/CollapseBar.razor.js
@@ -0,0 +1,26 @@
+﻿import EventHandler from "../../_content/BootstrapBlazor/modules/event-handler.js"
+
+export function init(id) {
+    const el = document.getElementById(id);
+    if (el === null) {
+        return;
+    }
+
+    EventHandler.on(el, 'click', e => {
+        e.preventDefault();
+
+        const layout = document.querySelector('.layout');
+        if (layout) {
+            layout.classList.toggle('is-collapsed');
+        }
+    });
+}
+
+export function dispose(id) {
+    const el = document.getElementById(id);
+    if (el === null) {
+        return;
+    }
+
+    EventHandler.off(el, 'click');
+}
