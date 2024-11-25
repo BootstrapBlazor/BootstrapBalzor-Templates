2b161a26aab172b32aa84bae7f7a3222ea4247b1
Argo-AsicoTech <argo@live.ca>
2024-11-25 11:56:55 +0800

    feat: 增加固定标签页配置项

--- src/Server/BootstrapBlazorApp.Server/Components/Shared/MainLayout.razor.cs
+++ src/Server/BootstrapBlazorApp.Server/Components/Shared/MainLayout.razor.cs
@@ -16,6 +16,8 @@ public sealed partial class MainLayout
 
     private bool IsFixedHeader { get; set; } = true;
 
+    private bool IsFixedTabHeader { get; set; } = true;
+
     private bool IsFixedFooter { get; set; } = true;
 
     private bool IsFullSide { get; set; } = true;
