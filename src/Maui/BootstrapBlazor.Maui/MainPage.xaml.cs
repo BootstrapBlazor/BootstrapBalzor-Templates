564a68683da576aa22dad836ed4b226d8124dd58
Argo Zhang <argo@live.ca>
2025-04-09 15:55:11 +0800

    chore(Maui): add MAUI template (#23)
    * refactor: 增加 Maui 工程
    
    * chore: 更新模板内容

---
+++ src/Maui/BootstrapBlazor.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BootstrapBlazor.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
